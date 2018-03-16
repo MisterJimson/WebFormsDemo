8b40111aa40b72f83c8279950c85726c009c1922
Jason Rai <jason.c.rai@gmail.com>
2018-03-16 18:57:36 -0400

    added files, builds, runs and works :)

---
+++ WebForms/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Ooui;
+using WebForms.Pages;
+using Xamarin.Forms;
+
+namespace WebForms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize Xamarin.Forms
+            Forms.Init();
+
+            // Publish a root element to be displayed
+            UI.Publish("/", new HomePage().GetOouiElement());
+        }
+    }
+}
